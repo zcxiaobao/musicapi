8029807d57c5b87c266a038f5f5b1f4e6beccbcd
zcxiaobao <444186976@qq.com>
2020-03-04 23:06:33 +0800

    1

--- app/index.js
+++ app/index.js
@@ -4,6 +4,8 @@ const app = new Koa()
 const routing = require('./routes')
 const parameter = require('koa-parameter')
 const cors = require('koa2-cors')
+
+app.use(bodyparser())
 app.use(async (ctx, next)=> {
   ctx.set('Access-Control-Allow-Origin', '*');
   ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
@@ -14,10 +16,8 @@ app.use(async (ctx, next)=> {
     await next();
   }
 });
-app.use(bodyparser())
-
 app.use(parameter(app))
 routing(app)
-app.listen(3000, () => {
+app.listen(3000,'0.0.0.0', () => {
   console.log('服务器启动于3000端口')
 })
