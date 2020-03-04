d587c89b2b4a6c7b255013efdb21c474169b01e9
zcxiaobao <444186976@qq.com>
2020-03-04 21:19:38 +0800

    try

--- app/index.js
+++ app/index.js
@@ -4,9 +4,18 @@ const app = new Koa()
 const routing = require('./routes')
 const parameter = require('koa-parameter')
 const cors = require('koa2-cors')
-
+app.use(async (ctx, next)=> {
+  ctx.set('Access-Control-Allow-Origin', '*');
+  ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  if (ctx.method == 'OPTIONS') {
+    ctx.body = 200; 
+  } else {
+    await next();
+  }
+});
 app.use(bodyparser())
-app.use(cors());
+
 app.use(parameter(app))
 routing(app)
 app.listen(3000, () => {
