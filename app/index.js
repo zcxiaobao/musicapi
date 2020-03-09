2db7beeea6127c6d93e238477d8068b1389df45d
zcxiaobao <444186976@qq.com>
2020-03-09 14:19:41 +0800

    koa跨域

--- app/index.js
+++ app/index.js
@@ -6,16 +6,34 @@ const parameter = require('koa-parameter')
 const cors = require('koa2-cors')
 
 app.use(bodyparser())
-app.use(async (ctx, next)=> {
-  ctx.set('Access-Control-Allow-Origin', '*');
-  ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-  ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-  if (ctx.method == 'OPTIONS') {
-    ctx.body = 200; 
-  } else {
-    await next();
-  }
-});
+const allowOrigins = [
+  "http://localhost:8080",
+  "http://47.240.82.152:8080"
+];
+app.use(cors({
+  origin: function(ctx) {
+    if (allowOrigins.includes(ctx.header.origin)) {
+      return ctx.header.origin;
+    }
+    return false;
+  },
+  exposeHeaders: ['WWW-Authenticate', 'Server-Authorization'],
+  maxAge: 5,
+  credentials: true,
+  withCredentials:true,
+  allowMethods: ['GET', 'POST', 'DELETE'],
+  allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
+}));
+// app.use(async (ctx, next)=> {
+//   ctx.set('Access-Control-Allow-Origin', '*');
+//   ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+//   ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+//   if (ctx.method == 'OPTIONS') {
+//     ctx.body = 200; 
+//   } else {
+//     await next();
+//   }
+// });
 app.use(parameter(app))
 routing(app)
 app.listen(8000, () => {
